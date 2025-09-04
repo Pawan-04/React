d592d2b66c4cf59b2f9f0b8816665c0fbc00859c
Pawan Kumar Pandey <pawan4kp@gmail.com>
2025-09-05 01:44:42 +0530

    Day3 work done

--- Day3/happydiwali/src/App.jsx
+++ Day3/happydiwali/src/App.jsx
@@ -1,10 +1,45 @@
 
 const App = () => {
+
+// Java script logic .....
+//.....
+//......
+//......
+
+
+
+const hello = ()=>{
+  alert("Hello world!!")
+}
+
+let bye = (message)=>{alert(message)}
+
+
   return (
-    <div>
-      
-    </div>
+    <>
+    <button onClick={hello}>click</button>  
+    <button onClick={() => bye("bye World!!")}>Click2</button>
+    </>
+
   )
 }
 
 export default App
+
+
+
+
+
+
+
+
+
+
+
+
+//function call replace itself with it's return value
+//function component always return HTML
+
+//we can not write anything after return
+//we can only return single data/entity/variable/value/etc
+//there must be a single return in a function and that must be the last statement
